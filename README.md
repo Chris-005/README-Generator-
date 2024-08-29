98aa07da1a0221060b246ad1a5867fb71a351b3d
Christian Guzman <cguzman199@gmail.com>
2024-08-28 21:30:51 -0500

    Adding code

---
+++ README.md
@@ -0,0 +1,24 @@
+# A readme generator
+    ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+    ## Description
+    A generator for any readme that you are trying to create
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution](#contribution)
+    * [Tests](#tests)
+    * [License](#license)
+    * [Questions](#questions)
+    ## Installation
+    install fs and inquirer
+    ## Usage
+    to help user create a readme by asking questions on how it will look
+    ## Contribution
+    idk
+    ## Tests
+    idk
+    ## License
+    This project is licensed under the MIT license.
+    ## Questions
+    If you have any questions, please feel free to contact me at cguzman199@gmail.com.
+    You can also find me on GitHub at [chris]
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license:',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data
+) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+}
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', generateMarkdown(data));
+        });
+}
 // Function call to initialize app
 init();
