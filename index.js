03d07c596a8f6be068a40186ee1a12c4a36053f1
Christian Guzman <cguzman199@gmail.com>
2024-08-28 21:48:52 -0500

    edit index

--- index.js
+++ index.js
@@ -53,14 +53,6 @@ const questions = [
     }
 ];
 
-## Table of Contents
-* [Installation](#installation)
-* [Usage](#usage)
-* [Contribution](#contribution)
-* [Tests](#tests)
-* [Questions](#questions)
-* [License](#license)
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data
 ) {
